1a68a2a0a6b4eca1b81fd25be7dd4088c0a32920
Thais Freitas <thais.freitas@enernoc.com>
2017-01-04 11:02:02 -0300

    Remove ignored files

--- test/hello_test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect;
-var greater = require('../lib/greater');
-
-description('Hello script',  function () {
-  it('returns hello', function (){
-    expect(greater()).to.equal('hello');
-  });
-});
