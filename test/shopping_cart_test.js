d09939ed916ad6750081eeba0e343d4172677a85
Thais Freitas <thais.freitas@enernoc.com>
2017-01-10 16:55:51 -0300

    add new test scenario for add item into the shopping cart

--- test/shopping_cart_test.js
+++ test/shopping_cart_test.js
@@ -6,4 +6,10 @@ describe('ShoppingCart', function() {
     var shoppingCart = ShoppingCart();
     expect(shoppingCart.items.length).to.be.equal(0);
   });
+  it('add item to shopping cart', function(){
+    var item = {name: 'agua de coco', price: 10};
+    var shoppingCart = ShoppingCart();
+    shoppingCart.add(item);
+    expect(shoppingCart.items.length).to.be.equal(1);
+  });
 });
